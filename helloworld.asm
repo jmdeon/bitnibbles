bce7b9132ab4df937005f0f47afe07f9e2b663cc
JosephDeon <jmdeon@gmail.com>
2017-01-12 10:32:29 -0800

    create hello world example that loops 8 times

---
+++ helloworld.asm
@@ -0,0 +1,28 @@
+org 32768
+
+start:
+ld      a, 2                  ; channel 2 = "S" for screen
+ld      b, 8                  ; channel 2 = "S" for screen
+call    $1601                 ; Select print channel using ROM
+
+loop:
+ld hl,line                    ; Print line
+call printline
+dec b
+jp nz, loop
+ret                        
+
+printline:                     ; Routine to print out a line
+ld a,(hl)                     ; Get character to print
+cp '$'                        ; See if it '$' terminator
+jp z,printend                 ; We're done if it is
+rst 16                        ; Spectrum: Print the character in 'A'
+inc hl                        ; Move onto the next character
+jp printline                  ; Loop round
+
+printend:
+ret
+
+; Data
+line:    defb 'Hello, world.',13,'$'
+
