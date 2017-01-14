c67106d20397abd7a56b0cd457a78f48e3c1fe00
Brando117 <bnpowers@ucsd.edu>
2017-01-14 14:35:29 -0800

    Cleaned up assembly
    Is there a blue dot at the end for anybody?

--- demos/moving.asm
+++ demos/moving.asm
@@ -1,224 +1,137 @@
-org 32768
+    org 32768
 
-ld hl, 22560 ;draw black over the bytes message
-ld b, 13
+    ld hl, 22560 ;draw black over the bytes message
+    ld b, 13
 clearloop:
-  ld (hl), 0
-  inc hl
-  dec b
-  jp nz, clearloop
+    ld (hl), 0
+    inc hl
+    dec b
+    jp nz, clearloop
+    jp start
 
-;22528 or hex 0x5800 is the start of the screen, goes until 
-;decimal 23296
+    ;22528 or hex 0x5800 is the start of the screen, goes until 
+    ;decimal 23296
 
-
-;draw H-left
-ld hl, 22592
-ld b,8
-h0loop:
-ld (hl), 28
-push bc ; save our b counter
-call wait
-pop bc
-;ld (hl), 0
-ld de, 32
-add hl, de
-dec b
-jp nz, h0loop
-
-;draw H-right
-ld hl, 22596
-ld b,8
-h1loop:
-ld (hl), 28
-push bc ; save our b counter
-call wait
-pop bc
-;ld (hl), 0
-ld de, 32
-add hl, de
-dec b
-jp nz, h1loop
-
-;draw H-mid
-ld hl, 22688
-ld b, 4
-h2loop:
-ld (hl), 28
-push bc ; save our b counter
-call wait
-pop bc
-inc hl
-dec b
-jp nz, h2loop
-
-
-;draw E-stem
-ld hl, 22599
-ld b,8
-e0loop:
-ld (hl),28
-push bc
-call wait
-pop bc
-ld de, 32
-add hl, de
-dec b
-jp nz, e0loop
-
-;draw E-top
-ld hl, 22599
-ld b, 4
-e1loop:
-ld (hl), 28
-push bc ; save our b counter
-call wait
-pop bc
-inc hl
-dec b
-jp nz, e1loop
-
-
-;draw E-mid
-ld hl, 22695
-ld b, 4
-e2loop:
-ld (hl), 28
-push bc ; save our b counter
-call wait
-pop bc
-inc hl
-dec b
-jp nz, e2loop
+hold:
+    inc a
+    ld bc,$1fff ;max waiting time
+hold_loop:
+    dec bc
+    ld a,b
+    or c
+    jr nz, hold_loop
+    ret
+    
+    
+    
+horiz_loop:
+    ld (hl), 28
+    push bc ; save our b counter
+    call hold
+    pop bc
+    inc hl
+    dec b
+    jp nz, horiz_loop
+    ret
 
 
 
-;draw E-low
-ld hl, 22823
-ld b, 4
-e3loop:
-ld (hl), 28
-push bc ; save our b counter
-call wait
-pop bc
-inc hl
-dec b
-jp nz, e3loop
+
+vertical_loop:
+    ld (hl), 28
+    push bc ; save our b counter
+    call hold
+    pop bc
+    ;ld (hl), 0
+    ld de, 32
+    add hl, de
+    dec b
+    jp nz, vertical_loop
+    ret
 
 
-;draw L-stem
-ld hl, 22605
-ld b,8
-l0loop:
-ld (hl),28
-push bc
-call wait
-pop bc
-ld de, 32
-add hl, de
-dec b
-jp nz, l0loop
+    
+    ;draw H-left
+start:
+    ld hl, 22592
+    ld b,8
+    call vertical_loop
+    
+    ;draw H-right
+    ld hl, 22596
+    ld b,8
+    call vertical_loop
+    
+    
+    ;draw H-mid
+    ld hl, 22688
+    ld b, 4
+    call horiz_loop
+    
 
 
-;draw L-low
-ld hl, 22829
-ld b, 4
-l1loop:
-ld (hl), 28
-push bc ; save our b counter
-call wait
-pop bc
-inc hl
-dec b
-jp nz, l1loop
+    ;draw E-stem
+    ld hl, 22599
+    ld b,8
+    call vertical_loop
+
+    ;draw E-top
+    ld hl, 22599
+    ld b, 4
+    call horiz_loop
 
 
-;draw L-stem
-ld hl, 22611
-ld b,8
-l2loop:
-ld (hl),28
-push bc
-call wait
-pop bc
-ld de, 32
-add hl, de
-dec b
-jp nz, l2loop
+    ;draw E-mid
+    ld hl, 22695
+    ld b, 4
+    call horiz_loop
 
 
-;draw L-low
-ld hl, 22835
-ld b, 4
-l3loop:
-ld (hl), 28
-push bc ; save our b counter
-call wait
-pop bc
-inc hl
-dec b
-jp nz, l3loop
+    ;draw E-low
+    ld hl, 22823
+    ld b, 4
+    call horiz_loop
+
+    ;draw L-stem
+    ld hl, 22605
+    ld b,8
+    call vertical_loop
 
 
-;draw O-stem 1
-ld hl, 22617
-ld b,8
-O0loop:
-ld (hl),28 
-push bc
-call wait
-pop bc
-ld de, 32
-add hl, de 
-dec b
-jp nz, O0loop
+
+    ;draw L-low
+    ld hl, 22829
+    ld b, 4
+    call horiz_loop
+
+    ;draw L-stem
+    ld hl, 22611
+    ld b,8
+    call vertical_loop
+
+    ;draw L-low
+    ld hl, 22835
+    ld b, 4
+    call horiz_loop
 
 
-;draw O-low
-ld hl, 22841
-ld b, 4
-O1loop:
-ld (hl), 28
-push bc ; save our b counter
-call wait
-pop bc
-inc hl
-dec b
-jp nz, O1loop
+    ;draw O-stem 1
+    ld hl, 22617
+    ld b,8
+    call vertical_loop
 
-;draw O-high
-ld hl, 22617
-ld b, 4
-O2loop:
-ld (hl), 28
-push bc ; save our b counter
-call wait
-pop bc
-inc hl
-dec b
-jp nz, O2loop
+    ;draw O-low
+    ld hl, 22841
+    ld b, 4
+    call horiz_loop
+
+    ;draw O-high
+    ld hl, 22617
+    ld b, 4
+    call horiz_loop
 
 
-;draw O-stem 2
-ld hl, 22621
-ld b,8
-O3loop:
-ld (hl),28
-push bc
-call wait
-pop bc
-ld de, 32
-add hl, de
-dec b
-jp nz, O3loop
-
-
-wait:
-       inc a
-       ld bc,$1fff ;max waiting time
-loop:
-	dec bc
-	ld a,b
-	or c
-	jr nz, loop
-        ret
+    ;draw O-stem 2
+    ld hl, 22621
+    ld b,8
+    call vertical_loop
