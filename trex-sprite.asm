1aa9d6a07ddcb48c58d3f9da6cefe75d27bec9c9
Brandon Powers <bnpowers@ucsd.edu>
2017-02-23 20:58:12 -0800

    Duplicated every row

--- trex-sprite.asm
+++ trex-sprite.asm
@@ -1,30 +1,50 @@
 trex:
         ;; ROW 1
         DEFB $00, $00, $03, $FF, $FC
+        DEFB $00, $00, $03, $FF, $FC
         DEFB $00, $00, $0F, $3F, $FF
         DEFB $00, $00, $0F, $3F, $FF
+        DEFB $00, $00, $0F, $3F, $FF
+        DEFB $00, $00, $0F, $3F, $FF
+        DEFB $00, $00, $0F, $FF, $FF
         DEFB $00, $00, $0F, $FF, $FF
         
         ;; ROW 2
         DEFB $00, $00, $0F, $FF, $FF
+        DEFB $00, $00, $0F, $FF, $FF
+        DEFB $00, $00, $0F, $FC, $00
         DEFB $00, $00, $0F, $FC, $00
         DEFB $00, $00, $0F, $FF, $F0
+        DEFB $00, $00, $0F, $FF, $F0
+        DEFB $C0, $00, $3F, $F0, $00
         DEFB $C0, $00, $3F, $F0, $00
         
         ;; ROW 3
         DEFB $C0, $00, $FF, $F0, $00
+        DEFB $C0, $00, $FF, $F0, $00
+        DEFB $F0, $0F, $FF, $FF, $00
         DEFB $F0, $0F, $FF, $FF, $00
         DEFB $FC, $3F, $FF, $F3, $00
+        DEFB $FC, $3F, $FF, $F3, $00
+        DEFB $FF, $FF, $FF, $F0, $00
         DEFB $FF, $FF, $FF, $F0, $00
         
         ;; ROW 4
         DEFB $3F, $FF, $FF, $F0, $00
+        DEFB $3F, $FF, $FF, $F0, $00
+        DEFB $0F, $FF, $FF, $C0, $00
         DEFB $0F, $FF, $FF, $C0, $00
         DEFB $00, $FF, $FF, $00, $00
+        DEFB $00, $FF, $FF, $00, $00
+        DEFB $00, $FC, $FC, $00, $00
         DEFB $00, $FC, $FC, $00, $00
         
         ;; ROW 5
         DEFB $00, $3F, $3C, $00, $00
+        DEFB $00, $3F, $3C, $00, $00
+        DEFB $00, $3C, $0C, $00, $00
         DEFB $00, $3C, $0C, $00, $00
         DEFB $00, $30, $0C, $00, $00
+        DEFB $00, $30, $0C, $00, $00
+        DEFB $00, $3C, $0F, $00, $00
         DEFB $00, $3C, $0F, $00, $00
