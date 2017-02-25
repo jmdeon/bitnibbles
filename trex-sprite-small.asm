5359eea9223fe90e1cc22decf538fbf49d09ac5a
Brandon Powers <bnpowers@ucsd.edu>
2017-02-24 16:53:13 -0800

    Fixed model

--- trex-sprite-small.asm
+++ trex-sprite-small.asm
@@ -5,23 +5,23 @@ trex:
         defb $00, $00, $00
         defb $00, $00, $00
         defb $00, $1f, $e0
-        defb $00, $27, $f0
-        defb $00, $2f, $f0
-        defb $00, $2f, $f0
+        defb $00, $37, $f0
+        defb $00, $3f, $f0
+        defb $00, $3f, $f0
         
         ;; ROW 2
-        defb $00, $2f, $f0
-        defb $00, $2e, $00
-        defb $00, $2f, $c0
+        defb $00, $3f, $f0
+        defb $00, $3e, $00
+        defb $00, $3f, $c0
         defb $80, $7c, $00
         defb $80, $fc, $00
-        defb $c2, $ff, $00
+        defb $c3, $ff, $00
         defb $e7, $fd, $00
         defb $ff, $fc, $00
         
         ;; ROW 3
         defb $7f, $fc, $00
-        defb $2f, $f8, $00
+        defb $3f, $f8, $00
         defb $0f, $f0, $00
         defb $0f, $e0, $00
         defb $07, $60, $00
