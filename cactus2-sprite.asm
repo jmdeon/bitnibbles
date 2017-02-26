f91b5ae2012bf02a55eb0855f3af5012fbd10445
Brandon Powers <bnpowers@ucsd.edu>
2017-02-25 17:44:23 -0800

    Added a cactus sprite

---
+++ cactus2-sprite.asm
@@ -0,0 +1,31 @@
+cact2:
+        defb 24, 3
+        ;; ROW 1
+        defb $00, $17, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $06, $3c, $40
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        
+        ;; ROW 2
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        defb $07, $ff, $c0
+        defb $03, $ff, $80
+        
+        ;; ROW 3
+        defb $01, $ff, $00
+        defb $00, $fc, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
