9a84e1a3463937ea0466f98b0aa0e190150e76b4
Brandon Powers <bnpowers@ucsd.edu>
2017-03-02 21:26:42 -0800

    Update Project and Sprites

--- project/project.asm
+++ project/project.asm
@@ -6,23 +6,136 @@ start:
   call draw_dino_init
   call draw_no_internet
   call pause_loop_spacebar
-  call GAME_LOADING
   call GAME_LOOP
-  ;jp forever_loop
-  ret
-
-GAME_LOADING:
-  ;call animate_landscape
-  ;call draw_high_score
-  ld hl, jmp_index
-  ld (hl), 0
   ret
 
 GAME_LOOP:
     call jump_iterate
     call hold
+    call cact_1
+    call hold
+    call cact_2
+    call hold
+    call cact_3
+    call hold
+    call cact_4
+    call hold
+    call cact_5
     jp GAME_LOOP
   ret
+  
+  
+  
+hold:
+    inc a
+    ld bc,$02ff         ; max waiting time. Why?
+hold_loop:
+    dec bc              ; Need to use bc? Use another register?
+    ld a,b
+    or c
+    jr nz, hold_loop
+    ret
+  
+
+pos: defb $a0
+
+dino_1:
+  ld hl, trex_right_up
+  ld b, 60
+  ld c, 16
+  call draw_bitmap
+  ret
+  
+dino_2:
+  
+  ld hl, trex_right_up
+  ld b, 60
+  ld c, 16
+  call delete_bitmap
+  
+  ld hl, trex_left_up
+  ld b, 60
+  ld c, 16
+  call draw_bitmap
+  ret
+
+dino_3:
+  
+  ld hl, trex_left_up
+  ld b, 60
+  ld c, 16
+  call delete_bitmap
+  ret
+  
+
+
+cact_1:
+  ld b, 60
+  ld hl, pos
+  ld c, (hl)
+  ld hl, cact2_1
+  call draw_bitmap
+  ret
+  
+cact_2:
+
+  ld b, 60
+  ld hl, pos
+  ld c, (hl)
+  ld hl, cact2_1
+  call delete_bitmap
+
+  ld b, 60
+  ld hl, pos
+  ld c, (hl)
+  ld hl, cact2_2
+  call draw_bitmap
+  ret
+  
+cact_3:
+
+  ld b, 60
+  ld hl, pos
+  ld c, (hl)
+  ld hl, cact2_2
+  call delete_bitmap
+  
+  ld b, 60
+  ld hl, pos
+  ld c, (hl)
+  ld hl, cact2_3
+  call draw_bitmap
+  ret
+  
+cact_4:
+  
+  ld b, 60
+  ld hl, pos
+  ld c, (hl)
+  ld hl, cact2_3
+  call delete_bitmap
+  
+  ld b, 60
+  ld hl, pos
+  ld c, (hl)
+  ld hl, cact2_4
+  call draw_bitmap
+  ret
+  
+cact_5:
+  
+  ld b, 60
+  ld hl, pos
+  ld c, (hl)
+  ld hl, cact2_4
+  call delete_bitmap
+  
+  ld hl, pos
+  ld a, (hl)
+  sub a, 8
+  ld (hl), a
+  ret
+
 
 jmp_positions:
   defb 60, 70, 80, 100, 105, 109, 109, 105, 100, 80, 70, 60
@@ -55,7 +168,7 @@ jmp_index_not_8:
   ld b, (hl)
 jmp_next_index: ;THIS IS WEHERE IT GETS REALLY WEIRD
   call jmp_load_b         ;b = jmp_positions[old_index]
-  ld hl, trex
+  ld hl, trex_stand
   ld c, 16
   call delete_bitmap      ;delete the previous trex
   ld hl, jmp_index
@@ -63,7 +176,7 @@ jmp_next_index: ;THIS IS WEHERE IT GETS REALLY WEIRD
   inc b
   ld (hl), b              ;save the new index value
   call jmp_load_b
-  ld hl, trex
+  ld hl, trex_stand
   ld c, 16
   call draw_bitmap        ;draw the trex in the new position
 jmp_end:
@@ -95,22 +208,10 @@ init_high_score:
 
 ;Initalize and draw the Trex in its starting position
 draw_dino_init:
-  ld hl, trex
+  ld hl, trex_stand
   ld c, 16     ;;X coordinate of top-left of initlal trex posiiton
   ld b, 60     ;;Y coordinate of top-left of initial trex position
   call draw_bitmap ;;Draw the dino
-  ;ld de, trex
-  ;ld c, 16     ;;X coordinate of top-left of initlal trex posiiton
-  ;ld b, 100     ;;Y coordinate of top-left of initial trex position
-  ;call draw_bitmap 
-  ld hl, cactus
-  ld c, 40     ;;X coordinate of top-left of initlal trex posiiton
-  ld b, 60    ;;Y coordinate of top-left of initial trex position
-  call draw_bitmap 
-  ;ld de, trex
-  ;ld c, 50
-  ;ld b, 100
-  ;call draw_bitmap
   ret
 
 ;Draw the no internet string at the bottom of the screen
@@ -154,6 +255,9 @@ draw_high_score:
 high_score_example:
   defb '0000'
 
+  
+  
+  
 ;hl bitmap addr
 ;b   loop counter
 ;bc' x,y
@@ -269,7 +373,18 @@ delete_row_loop:
 high_score_value:
         defw $00
 
-trex:
+
+;hold for a brief moment
+
+    
+
+;Lock the program in a loop which lasts forever
+forever_loop:
+  jp forever_loop
+
+  
+  
+trex_stand:
         ;; ROW 1
         defb $00, $00, $00
         defb $00, $00, $00
@@ -299,8 +414,70 @@ trex:
         defb $06, $20, $00
         defb $04, $20, $00
         defb $06, $30, $00
+        
+trex_left_up:
+        ;; ROW 1
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $1f, $e0
+        defb $00, $37, $f0
+        defb $00, $3f, $f0
+        defb $00, $3f, $f0
+        
+        ;; ROW 2
+        defb $00, $3f, $f0
+        defb $00, $3e, $00
+        defb $00, $3f, $c0
+        defb $80, $7c, $00
+        defb $80, $fc, $00
+        defb $c3, $ff, $00
+        defb $e7, $fd, $00
+        defb $ff, $fc, $00
+        
+        ;; ROW 3
+        defb $7f, $fc, $00
+        defb $3f, $f8, $00
+        defb $0f, $f0, $00
+        defb $0f, $e0, $00
+        defb $06, $60, $00
+        defb $03, $20, $00
+        defb $00, $20, $00
+        defb $00, $30, $00
+        
+trex_right_up:
+        ;; ROW 1
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $1f, $e0
+        defb $00, $37, $f0
+        defb $00, $3f, $f0
+        defb $00, $3f, $f0
+        
+        ;; ROW 2
+        defb $00, $3f, $f0
+        defb $00, $3e, $00
+        defb $00, $3f, $c0
+        defb $80, $7c, $00
+        defb $80, $fc, $00
+        defb $c3, $ff, $00
+        defb $e7, $fd, $00
+        defb $ff, $fc, $00
+        
+        ;; ROW 3
+        defb $7f, $fc, $00
+        defb $3f, $f8, $00
+        defb $0f, $f0, $00
+        defb $0f, $e0, $00
+        defb $07, $30, $00
+        defb $06, $00, $00
+        defb $04, $00, $00
+        defb $06, $00, $00
 
-cactus:
+cact2_1:
         ;; ROW 1
         defb $00, $06, $00
         defb $00, $0f, $00
@@ -331,17 +508,102 @@ cactus:
         defb $00, $0f, $00
         defb $00, $0f, $00
 
-;hold for a brief moment
-hold:
-    inc a
-    ld bc,$1fff         ; max waiting time. Why?
-hold_loop:
-    dec bc              ; Need to use bc? Use another register?
-    ld a,b
-    or c
-    jr nz, hold_loop
-    ret
+;; 2nd part of animation
+cact2_2:
+        ;; ROW 1
+        defb $00, $18, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $06, $3c, $40
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        
+        ;; ROW 2
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        defb $0f, $3c, $e0
+        defb $0f, $ff, $e0
+        defb $07, $ff, $c0
+        
+        ;; ROW 3
+        defb $01, $ff, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        defb $00, $3c, $00
+        
+        
 
-;Lock the program in a loop which lasts forever
-forever_loop:
-  jp forever_loop
+;; 3rd part of animation
+cact2_3:
+        ;; ROW 1
+        defb $00, $60, $00
+        defb $00, $f0, $00
+        defb $00, $f0, $00
+        defb $00, $f0, $00
+        defb $00, $f0, $00
+        defb $18, $f1, $00
+        defb $3c, $f3, $80
+        defb $3c, $f3, $80
+        
+        ;; ROW 2
+        defb $3c, $f3, $80
+        defb $3c, $f3, $80
+        defb $3c, $f3, $80
+        defb $3c, $f3, $80
+        defb $3c, $f3, $80
+        defb $3c, $f3, $80
+        defb $3f, $ff, $80
+        defb $1f, $ff, $00
+        
+        ;; ROW 3
+        defb $07, $fc, $00
+        defb $00, $f0, $00
+        defb $00, $f0, $00
+        defb $00, $f0, $00
+        defb $00, $f0, $00
+        defb $00, $f0, $00
+        defb $00, $f0, $00
+        defb $00, $f0, $00
+        
+        
+;; 4th part of animation
+cact2_4:
+        ;; ROW 1
+        defb $01, $80, $00
+        defb $03, $c0, $00
+        defb $03, $c0, $00
+        defb $03, $c0, $00
+        defb $03, $c0, $00
+        defb $63, $c4, $00
+        defb $f3, $ce, $00
+        defb $f3, $ce, $00
+        
+        ;; ROW 2
+        defb $f3, $ce, $00
+        defb $f3, $ce, $00
+        defb $f3, $ce, $00
+        defb $f3, $ce, $00
+        defb $f3, $ce, $00
+        defb $f3, $ce, $00
+        defb $ff, $fe, $00
+        defb $7f, $fc, $00
+        
+        ;; 
+        defb $1f, $f0, $00
+        defb $03, $c0, $00
+        defb $03, $c0, $00
+        defb $03, $c0, $00
+        defb $03, $c0, $00
+        defb $03, $c0, $00
+        defb $03, $c0, $00
+        defb $03, $c0, $00
+  
