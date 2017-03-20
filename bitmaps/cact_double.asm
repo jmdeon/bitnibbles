304070c31c84758f3a336491b2d63c4f50789815
Brandon Powers <bnpowers@ucsd.edu>
2017-03-20 15:26:49 -0700

    New double cactus bitmap

---
+++ bitmaps/cact_double.asm
@@ -0,0 +1,64 @@
+cact_double_1:
+        ;; ROW 1
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        
+        ;; ROW 2
+        defb $00, $08, $10
+        defb $01, $c0, $38
+        defb $01, $c0, $b8
+        defb $01, $c1, $b8
+        defb $05, $d1, $ba
+        defb $0d, $d9, $bb
+        defb $0d, $d9, $bb
+        defb $0d, $d9, $bb
+        
+        ;; ROW 3
+        defb $0d, $d9, $fb
+        defb $0d, $d9, $fb
+        defb $0f, $f8, $3f
+        defb $07, $f0, $3e
+        defb $01, $c0, $38
+        defb $01, $c0, $38
+        defb $01, $c0, $38
+        defb $01, $c0, $38
+
+
+cact_double_2:
+        ;; ROW 1
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        defb $00, $00, $00
+        
+        ;; ROW 2
+        defb $08, $01, $00
+        defb $1c, $03, $80
+        defb $1c, $0b, $80
+        defb $1c, $1b, $80
+        defb $5d, $1b, $a0
+        defb $dd, $9b, $b0
+        defb $dd, $9b, $b0
+        defb $dd, $9b, $b0
+        
+        ;; ROW 3
+        defb $dd, $9f, $b0
+        defb $dd, $8f, $b0
+        defb $ff, $83, $f0
+        defb $7f, $f0, $00
+        defb $1c, $03, $80
+        defb $1c, $03, $80
+        defb $1c, $03, $80
+        defb $1c, $03, $80
+        
+        
